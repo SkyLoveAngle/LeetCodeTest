6747835ce1140918871fd46600df02696f0eaa92
Qin-SKY <1920909528@qq.com>
2021-03-21 15:25:26 +0800

    给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。你可以返回满足此条件的任何数组作为答案。

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -2,31 +2,24 @@ package leetcode.editor.cn;
 
 //判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
 //
-// 示例 1: 
-//
+// 示例 1:
 // 输入: 121
 //输出: true
-// 
 //
-// 示例 2: 
-//
+// 示例 2:
 // 输入: -121
 //输出: false
 //解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
-// 
 //
-// 示例 3: 
-//
+// 示例 3:
 // 输入: 10
 //输出: false
 //解释: 从右向左读, 为 01 。因此它不是一个回文数。
-// 
 //
-// 进阶: 
-//
+// 进阶:
 // 你能不将整数转为字符串来解决这个问题吗？ 
 // Related Topics 数学 
-// 👍 1345 👎 0
+// 👍 1365 👎 0
 
 public class PalindromeNumber{
     public static void main(String[] args) {
@@ -37,14 +30,20 @@ public class PalindromeNumber{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0)return false;
-        int temp = 0;
-        int res = x;
-        while (res!=0){
-            temp = temp * 10 + res % 10;
-            res /= 10;
+        String str = ""+x;
+        int i = 0;
+        int j = str.length()-1;
+        while ( i < j){
+            if (str.charAt(i) == str.charAt(j)){
+                i++;
+                j--;
+            }else {
+                return false;
+            }
+
         }
-        return temp == x;
+        return true;
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
