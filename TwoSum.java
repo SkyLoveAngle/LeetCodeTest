a16d7d1b3b29d8d7518a0828242604b7adabf917
Qin-SKY <1920909528@qq.com>
2021-03-22 08:42:11 +0800

    给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。你可以假设除了整数 0 之外，这个整数不会以零开头。

--- TwoSum.java
+++ TwoSum.java
@@ -1,49 +1,48 @@
 package leetcode.editor.cn;
 
-//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
-//
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
-// 示例: 
+// 你可以按任意顺序返回答案。
+// 示例 1：
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+// 示例 2：
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+
+// 示例 3：
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
 //
-// 给定 nums = [2, 7, 11, 15], target = 9
-//
-//因为 nums[0] + nums[1] = 2 + 7 = 9
-//所以返回 [0, 1]
+// 提示：
+// 2 <= nums.length <= 103 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// 只会存在一个有效答案 
+// 
+// Related Topics 数组 哈希表 
+// 👍 10157 👎 0
+
+public class TwoSum{
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+    }
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i ++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target-nums[i]),i};
+            }
             map.put(nums[i], i);
         }
-        for (int i = 0; i < nums.length; i ++) {
-            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) {
-                result[0] = i;
-                result[1] = map.get(target - nums[i]);
-                break;
-            }
-        }
-        return result;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
-
-
-    //暴力法很简单，遍历每个元素 xx，并查找是否存在一个值与 target - xtarget−x 相等的目标元素。
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] + nums[i]) == target)  {
-                        return new int[] { i, j };
-                    }
-                }
-            }
-        // 当数组中没有符合要求的两个元素时，抛出异常
-            throw new IllegalArgumentException("No two sum solution");
-        }
-    }
 
 }
