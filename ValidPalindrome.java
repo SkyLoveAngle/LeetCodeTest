e1c7ff911686ab221e8a827fa336a7096c761515
Qin-SKY <1920909528@qq.com>
2021-03-31 09:05:37 +0800

    给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。

---
+++ ValidPalindrome.java
@@ -0,0 +1,49 @@
+package leetcode.editor.cn;
+
+//给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+// 说明：本题中，我们将空字符串定义为有效的回文串。 
+//
+// 示例 1:
+// 输入: "A man, a plan, a canal: Panama"
+//输出: true
+//
+// 示例 2:
+// 输入: "race a car"
+//输出: false
+// 
+// Related Topics 双指针 字符串 
+// 👍 320 👎 0
+
+public class ValidPalindrome{
+    public static void main(String[] args) {
+        Solution solution = new ValidPalindrome().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+
+        int n = sgood.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            if (Character.toLowerCase(sgood.charAt(left)) != Character.toLowerCase(sgood.charAt(right))) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
