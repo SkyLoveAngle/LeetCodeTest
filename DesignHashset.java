90857cb1a9f52de1291014e70821716febce76db
Qin-SKY <1920909528@qq.com>
2021-06-24 09:08:53 +0800

    不使用任何内建的哈希表库设计一个哈希集合（HashSet）。实现 MyHashSet 类：

---
+++ DesignHashset.java
@@ -0,0 +1,136 @@
+package leetcode.editor.cn;
+
+// 不使用任何内建的哈希表库设计一个哈希集合（HashSet）。
+// 实现 MyHashSet 类：
+// void add(key) 向哈希集合中插入值 key 。 
+// bool contains(key) 返回哈希集合中是否存在这个值 key 。 
+// void remove(key) 将给定值 key 从哈希集合中删除。
+// 如果哈希集合中没有这个值，什么也不做。
+
+//示例：
+//输入：
+//["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+//[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+//输出：
+//[null, null, null, true, false, null, true, null, false]
+//
+//解释：
+//MyHashSet myHashSet = new MyHashSet();
+//myHashSet.add(1);      // set = [1]
+//myHashSet.add(2);      // set = [1, 2]
+//myHashSet.contains(1); // 返回 True
+//myHashSet.contains(3); // 返回 False ，（未找到）
+//myHashSet.add(2);      // set = [1, 2]
+//myHashSet.contains(2); // 返回 True
+//myHashSet.remove(2);   // set = [1]
+//myHashSet.contains(2); // 返回 False ，（已移除） 
+
+// 提示：
+// 0 <= key <= 106 
+// 最多调用 104 次 add、remove 和 contains 。
+// 进阶：你可以不使用内建的哈希集合库解决此问题吗？ 
+// Related Topics 设计 哈希表 
+// 👍 153 👎 0
+
+public class DesignHashset {
+    public static void main(String[] args) {
+        Solution solution = new DesignHashset().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class MyHashSet {
+        // 由于使用的是「链表」，这个值可以取得很小
+        Node[] nodes = new Node[10009];
+
+        public void add(int key) {
+            // 根据 key 获取哈希桶的位置
+            int idx = getIndex(key);
+            // 判断链表中是否已经存在
+            Node loc = nodes[idx], tmp = loc;
+            if (loc != null) {
+                Node prev = null;
+                while (tmp != null) {
+                    if (tmp.key == key) {
+                        return;
+                    }
+                    prev = tmp;
+                    tmp = tmp.next;
+                }
+                tmp = prev;
+            }
+            Node node = new Node(key);
+
+            // 头插法
+            // node.next = loc;
+            // nodes[idx] = node;
+
+            // 尾插法
+            if (tmp != null) {
+                tmp.next = node;
+            } else {
+                nodes[idx] = node;
+            }
+        }
+
+        public void remove(int key) {
+            int idx = getIndex(key);
+            Node loc = nodes[idx];
+            if (loc != null) {
+                Node prev = null;
+                while (loc != null) {
+                    if (loc.key == key) {
+                        if (prev != null) {
+                            prev.next = loc.next;
+                        } else {
+                            nodes[idx] = loc.next;
+                        }
+                        return;
+                    }
+                    prev = loc;
+                    loc = loc.next;
+                }
+            }
+        }
+
+        public boolean contains(int key) {
+            int idx = getIndex(key);
+            Node loc = nodes[idx];
+            if (loc != null) {
+                while (loc != null) {
+                    if (loc.key == key) {
+                        return true;
+                    }
+                    loc = loc.next;
+                }
+            }
+            return false;
+        }
+
+        static class Node {
+            private int key;
+            private Node next;
+            private Node(int key) {
+                this.key = key;
+            }
+        }
+
+        int getIndex(int key) {
+            // 因为 nodes 的长度只有 10009，对应的十进制的 10011100011001（总长度为 32 位，其余高位都是 0）
+            // 为了让 key 对应的 hash 高位也参与运算，这里对 hashCode 进行右移异或
+            // 使得 hashCode 的高位随机性和低位随机性都能体现在低 16 位中
+            int hash = Integer.hashCode(key);
+            hash ^= (hash >>> 16);
+            return hash % nodes.length;
+        }
+    }
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
